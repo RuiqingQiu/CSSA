c51acfb5496c0256b872a07a1a9014324dc8903b
Ruiqing Qiu <raymond.qiu1993@gmail.com>
2013-11-23 11:21:30 -0800

    indent fixed ruiqingqiu

--- src/com/cssa/app/qiu_rui_qing/qiu_rui_qing.java
+++ src/com/cssa/app/qiu_rui_qing/qiu_rui_qing.java
@@ -17,32 +17,26 @@ import android.view.View.OnClickListener;
 import android.widget.Button;
 
 public class qiu_rui_qing extends Activity {
-	protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.qiu_rui_qing);
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.qiu_rui_qing);
         
-        //A button for going to pool page
-        Button pool_info = (Button) findViewById(R.id.poolButton);
-        Button civitas_info = (Button) findViewById(com.cssa.app.R.id.FirstActivityButton);
-        pool_info.setOnClickListener(new OnClickListener(){
-
-			@Override
-			public void onClick(View arg0) {
-				Intent i = new Intent(getApplicationContext(), PoolPage.class);
-				startActivity(i);
-				
-			}
-        	
-        });
-        
-        civitas_info.setOnClickListener(new OnClickListener(){
-
-			@Override
-			public void onClick(View v) {
-				Intent i = new Intent(getApplicationContext(),CitivasPage.class);
-				startActivity(i);
-			}
-        	
-        });
-	}
+    //A button for going to pool page
+    Button pool_info = (Button) findViewById(R.id.poolButton);
+    Button civitas_info = (Button) findViewById(com.cssa.app.R.id.FirstActivityButton);
+    pool_info.setOnClickListener(new OnClickListener(){
+      @Override
+      public void onClick(View arg0) {
+        Intent i = new Intent(getApplicationContext(), PoolPage.class);
+        startActivity(i);
+      }
+    });
+    civitas_info.setOnClickListener(new OnClickListener(){
+      @Override
+      public void onClick(View v) {
+        Intent i = new Intent(getApplicationContext(),CitivasPage.class);
+        startActivity(i);
+      }
+    });
+  }
 }

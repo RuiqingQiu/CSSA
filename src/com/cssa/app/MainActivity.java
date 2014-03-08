feae440ca4642ae851b7dc870af34301aa8f9ee9
Ruiqing Qiu <raymond.qiu1993@gmail.com>
2014-03-08 13:07:32 -0800

    pre spring break alpha edition

--- src/com/cssa/app/MainActivity.java
+++ src/com/cssa/app/MainActivity.java
@@ -57,19 +57,10 @@ public class MainActivity extends Activity implements OnGestureListener{
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-<<<<<<< HEAD
-		//ActionBar bar = getActionBar();
-		//bar.setIcon(
-				   //new ColorDrawable(getResources().getColor(android.R.color.transparent))); 
-		//bar.setBackgroundDrawable(getResources().getDrawable(R.drawable.bbar_1080x144));
-=======
 		mainActivity = this;
 		ActionBar bar = getActionBar();
-		bar.setIcon(
-				   new ColorDrawable(getResources().getColor(android.R.color.transparent))); 
+		bar.setIcon(new ColorDrawable(getResources().getColor(android.R.color.transparent))); 
 		bar.setBackgroundDrawable(getResources().getDrawable(R.drawable.bbar_1080x144));
->>>>>>> FETCH_HEAD
-
 		FlipDetector = new GestureDetector(this,this);
 		//strict mode for internet access
 		StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();

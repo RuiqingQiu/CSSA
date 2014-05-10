4364570a4177303719486d50b756dca6ba90a088
Ruiqing Qiu <raymond.qiu1993@gmail.com>
2014-05-10 14:05:52 -0700

    card ui lib done

---
+++ src/com/cssa/app/qiu_rui_qing/MyCard.java
@@ -0,0 +1,23 @@
+package com.cssa.app.qiu_rui_qing;
+
+import android.view.View;
+import android.widget.TextView;
+import com.cssa.app.R;
+import com.fima.cardsui.objects.RecyclableCard;
+
+public class MyCard extends RecyclableCard {
+
+	public MyCard(String title){
+		super(title);
+	}
+
+	@Override
+	protected int getCardLayoutId() {
+		return R.layout.card_ex;
+	}
+
+	@Override
+	protected void applyTo(View convertView) {
+		((TextView) convertView.findViewById(R.id.title)).setText(title);
+	}
+}
---
+++ src/com/cssa/app/qiu_rui_qing/MyImageCard.java
@@ -0,0 +1,27 @@
+package com.cssa.app.qiu_rui_qing;
+
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.fima.cardsui.objects.RecyclableCard;
+import com.cssa.app.R;
+
+public class MyImageCard extends RecyclableCard {
+
+	public MyImageCard(String title, int image){
+		super(title, image);
+	}
+
+	@Override
+	protected int getCardLayoutId() {
+		return R.layout.card_picture;
+	}
+
+	@Override
+	protected void applyTo(View convertView) {
+		((TextView) convertView.findViewById(R.id.title)).setText(title);
+		((ImageView) convertView.findViewById(R.id.imageView1)).setImageResource(image);
+	}
+
+}
